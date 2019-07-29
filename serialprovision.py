6680c7ba8105f29489d3d3a0c666b0ec7c39214f
Cameron Lewis <46030732+cblewis@users.noreply.github.com>
2019-07-29 15:36:04 -0700

    Add files via upload

---
+++ serialprovision.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+import serial
+import time
+from getpass import getpass
+
+#Masterfully crafted by Cameron Lewis
+
+#check devmngr for serial port and its settings
+console = serial.Serial(
+    port="COM3",
+    baudrate="9600",
+    parity="N",
+    stopbits=1,
+    bytesize=8,
+    timeout=8
+)
+
+username = input("Username: ")
+password = getpass()
+#need to convert the input of these variables as bytes
+nameb = bytes(username, encoding= "utf-8")
+passb = bytes(password, encoding= "utf-8")
+
+console.write(b'\n')
+time.sleep(1)
+input_data = console.read(console.inWaiting())
+print(input_data)
+
+console.write(nameb)
+console.write(b'\n')
+#console.write("b'" + username + "\n'" + "b'\n'").encode() doesnt work
+time.sleep(1)
+input_data = console.read(console.inWaiting())
+print(input_data)
+
+console.write(passb)
+console.write(b'\n')
+#console.write("b'" + password + "\n'" + "b'\n'").encode() doesnt work
+time.sleep(2)
+input_data = console.read(console.inWaiting())
+print(input_data)
+
+console.write(b'\n')
+time.sleep(1)
+input_data = console.read(console.inWaiting())
+print(input_data)
+
+time.sleep(14)
+
+console.write(b'\n')
+time.sleep(1)
+input_data = console.read(console.inWaiting())
+print(input_data)
+
+#if we read as bytes 'rb', then we don't have to concatenate
+#every line in the config with b'line'\n like appendconfig.py
+filepath = 'test.txt'
+with open(filepath, 'rb') as f:
+    configLines = f.readlines()
+    for line in configLines:
+        print(line)
+        console.write(line)
+        time.sleep(2)
+        #console.write(b'\n')
+        input_data = console.read(console.inWaiting())
+        print(input_data)
+f.close()
+
+console.write(b'exit' + b'\n')
+input_data = console.read(console.inWaiting())
+print(input_data)
