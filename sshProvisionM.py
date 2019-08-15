a187d0aef9332bfee89fec3078f8f5c9767b4df4
Cameron Lewis <46030732+cblewis@users.noreply.github.com>
2019-08-15 16:25:48 -0700

    Create sshProvisionM.py

---
+++ sshProvisionM.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+from netmiko import Netmiko
+from getpass import getpass
+import time
+
+#Masterfully crafted by Cameron Lewis
+
+#Specify the device type:
+mrv = {
+    "host": input("IP: "),
+    "username": input("Username: "),
+    "password": getpass(),
+    "device_type": "mrv_optiswitch",
+    'global_delay_factor': 2,
+}
+
+confip = input("172.22.6.X address to be assigned to this device: ")
+enablepass = input("Enable password: ")
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+#Specify the device type here too:
+net_connect = Netmiko(**mrv)
+
+#See if enable password is required.
+login = net_connect.find_prompt()
+print(login)
+if r"Password" in login:
+    try:
+        print(net_connect.find_prompt())
+        output = net_connect.send_command(enablepass + r"\\n")
+        print(output)
+    except:
+        raise
+elif r">" in login:
+    try:
+        print(net_connect.find_prompt())
+        output = net_connect.send_command("enable", expect_string=r'>')
+        print(output)
+        print(net_connect.find_prompt())
+        output = net_connect.send_command(enablepass + r"\\n")
+        print(output)
+    except:
+        raise
+else:
+    print("No need for enable password.")
+
+print(net_connect.find_prompt())
+output = net_connect.send_command("conf t", expect_string=r'#')
+print(output)
+
+#Specify from which file commands will be read
+filepath = 'config.txt'
+with open(filepath, 'r') as f:
+    configLines = f.readlines()
+    for line in configLines:
+        print(net_connect.find_prompt())
+        print(line)
+        output = net_connect.send_command(line, expect_string=r'#')
+        print(output)
+        #time.sleep(4)
+f.close()
+
+#Configure new MGMT IP on device
+print(net_connect.find_prompt())
+output = net_connect.send_command("ip " + confip + "/22", expect_string=r'#')
+print("ip " + confip + "/22")
+print(output)
+
+#Save Changes
+savemem = "wr mem"
+print(net_connect.find_prompt())
+output = net_connect.send_command(savemem, expect_string=r'#')
+print(savemem)
+print(output)
+
+#Logout from the SSH connection.
+net_connect.disconnect()
